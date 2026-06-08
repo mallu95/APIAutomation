ae99808cdb380a4c08ed854081365039a59176c3
Admin <mallukuntoji95@gmail.com>
2026-06-08 20:38:43 +0530

    08-June_2026

---
+++ src/test/java/org/example/APITest_001.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class APITest_001 {
+    public static void main(String[] args) {
+        //Gherkin Syntax
+        //base Uri: https://restful-booker.herokuapp.com
+        //base path: /booking/1
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/booking/2")
+                .when()
+                    .get()
+                .then()
+                    .log().all()
+                    .statusCode(200);
+    }
+}
